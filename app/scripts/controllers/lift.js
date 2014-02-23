b5dcbe67822825ab1e9be3632755d2c831fee38a
Mike Munroe <mike@mikemunroe.com>
2014-02-23 12:16:02 -0500

    back out inline bracket controller notation

--- app/scripts/controllers/lift.js
+++ app/scripts/controllers/lift.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('fitrecordMeanApp')
-  .controller('LiftCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+  .controller('LiftCtrl', function ($scope, $http) {
       $http.get('lifts/lifts.json').success(function(data) {
         $scope.lifts = data;
     });
 
     $scope.orderProp = 'load';
-}]);
+});
